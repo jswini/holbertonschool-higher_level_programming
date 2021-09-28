29874041aafdb0f141369c9a1a07a6b5bfee9f92
jswini <jswinikoff@gmail.com>
2021-09-28 15:33:31 -0500

    fixed for pep8

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,15 +2,15 @@
 import random
 number = random.randint(-10000, 10000)
 zero = "0"
-greater_than_5 = "greater than 5"
-less_than_6 = "less than 6 and not 0"
+great5 = "greater than 5"
+less6 = "less than 6 and not 0"
 if number > 0:
-    last_digit = number % 10
+    last = number % 10
 else:
-    last_digit = number % -10
-if last_digit > 5:
-    print("Last digit of {} is {} and is {}".format(number, last_digit, greater_than_5))
-elif last_digit == 0:
-    print("Last digit of {} is {} and is {}".format(number, last_digit, zero))
+    last = number % -10
+if last > 5:
+    print("Last digit of {} is {} and is {}".format(number, last, great5))
+elif last == 0:
+    print("Last digit of {} is {} and is {}".format(number, last, zero))
 else:
-    print("Last digit of {} is {} and is {}".format(number, last_digit, less_than_6))
+    print("Last digit of {} is {} and is {}".format(number, last, less6))
