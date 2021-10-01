d8ee652ea34a6aff2f21cdc0f759238053e6e939
jswini <jswinikoff@gmail.com>
2021-10-01 13:42:57 -0500

    added condition for empty list

--- 0x02-python-data_structures/9-max_integer.py
+++ 0x02-python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    length = len(my_list)
-    my_list.sort()
-    return my_list[length - 1]
+    if not my_list:
+        return None
+    else:
+        length = len(my_list)
+        my_list.sort()
+        return my_list[length - 1]
