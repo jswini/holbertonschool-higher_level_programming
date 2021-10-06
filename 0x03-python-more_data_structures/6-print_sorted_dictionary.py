e84ab5b70f2814da5ff5ca6db4e8de5690ed1259
jswini <jswinikoff@gmail.com>
2021-10-06 11:13:28 -0500

    removed prints for debug

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    print(a_dictionary)
     my_dictionary = sorted(a_dictionary.keys())
     for i in my_dictionary:
         print("{}: {}".format(i, a_dictionary[i]))
