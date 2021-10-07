8ce33c6ff367e15a2b0a27d643dc89558691f663
jswini <jswinikoff@gmail.com>
2021-10-07 14:31:04 -0500

    first commit of uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = ""
+    for i in str:
+        tester = ord(i)
+        if tester >= 97 and tester <= 122:
+            tester -= 32
+        new_str += chr(tester)
+    print(new_str)
