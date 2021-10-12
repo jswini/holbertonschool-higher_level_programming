95a66837de64e27ee7a2994a61262adf11ae0a2a
jswini <jswinikoff@gmail.com>
2021-10-12 15:14:29 -0500

    first commit of square

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """
+    This class creates a square of a specific size
+    """
+    def __init__(self, size = 1):
+        """
+        Takes input of size and initializes an object of class size
+        """
+        self.__size = size
