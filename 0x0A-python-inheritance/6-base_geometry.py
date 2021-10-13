47738a8a5fcd60dbfdecb6cdde61c11d14272fbf
jswini <jswinikoff@gmail.com>
2021-10-13 15:26:05 -0500

    first commit of base geometry with not implemented area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module implemets 1 or more steps of the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This is a starter class for BaseGeometry
+    """
+
+    def area(self):
+        """
+        This is a non implemented area method
+        """
+        raise Exception("area() is not implemented")
