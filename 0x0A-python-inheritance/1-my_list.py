f8f9b429cbe53c6b7fd822a9c846b3fca5c0f70e
jswini <jswinikoff@gmail.com>
2021-10-13 14:47:12 -0500

    first commit of my_list subclass

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module creates a subclass of list with int data
+"""
+class MyList(list):
+    """
+    This instantiates the subclass of super class list
+    """
+    def __init__(self):
+        super().__init__()
+
+
+    def print_sorted(self):
+        """
+        This method sorts the list and then prints it
+        """
+        print(sorted(self))
