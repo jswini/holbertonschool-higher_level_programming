25e01af6f97db917b7db95f2827cf8ce402f1db4
jswini <jswinikoff@gmail.com>
2021-10-12 20:37:42 -0500

    first commit of 1-rectangle

---
+++ 0x06-python-more_classes/1-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+This will define a rectangle object
+"""
+
+
+class Rectangle:
+    """
+    This instantiates the rectangle
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        This returns the width property of our rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        This sets the width of the rectangle
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        This returns the height property of our rectangle
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        This sets the height of the rectangle
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        This module sets the area
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        This module sets the perimeter
+        """
+        return ((2 * self.__width) + (2 * self.__height))
