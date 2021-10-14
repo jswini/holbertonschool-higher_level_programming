8017219f8007b801b4e8478490d43ff6fc317427
jswini <jswinikoff@gmail.com>
2021-10-13 20:15:38 -0500

    first commit of rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module implemets 1 or more steps of the BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    This is a starter class for BaseGeometry
+    """
+
+    def area(self):
+        """
+        This is a non implemented area method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        This method checks if the input is a positive intedge
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    this subclass for base geometry creates a rectangle
+    """
+    def __init__(self, width, height):
+        """
+        this instantiates the object of the sub class
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator(width, self.__width)
+        self.integer_validator(height, self.__height)
