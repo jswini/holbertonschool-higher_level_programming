a779803c708bdf4a764a08db832fad3213497142
jswini <jswinikoff@gmail.com>
2021-10-14 10:41:16 -0500

    corrected to not use an if statement

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,4 @@ def inherits_from(obj, a_class):
     """
     we return true or false based on the test
     """
-    if obj == issubclass(type(obj), a_class) and type(obj) is not a_class:
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
