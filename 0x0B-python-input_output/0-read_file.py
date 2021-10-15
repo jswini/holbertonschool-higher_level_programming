6b42342cedebd9e420bfed36b824b4058c22601f
jswini <jswinikoff@gmail.com>
2021-10-15 15:37:11 -0500

    added comments

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,7 @@ this module reads from a file and outputs result to the screen
 """
 def read_file(filename=""):
     """
+    open read and print the file
     """
     with open("my_file_0.txt", encoding='utf-8') as my_read_file:
         print(my_read_file.read(), end="")
