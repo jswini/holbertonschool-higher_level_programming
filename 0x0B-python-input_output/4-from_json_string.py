8da6eb7c43ba96707ccf36b5a6c6c811023b9c81
jswini <jswinikoff@gmail.com>
2021-10-19 13:22:13 -0500

    added comments

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
+deserializes a json
 """
 import json
 
 
 def from_json_string(my_str):
     """
+    this is the command that completes the task
     """
     return json.loads(my_str)
