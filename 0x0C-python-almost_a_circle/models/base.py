098db5e2dd6eca7cbab0e4760e978481821913f9
jswini <jswinikoff@gmail.com>
2021-10-20 13:28:04 -0500

    First commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+class Base:
+    """
+    This is the base class for almost a circle
+    """
+
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        This method instantiates an object of class base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
