f11840a18697c152224a57a7bed07a931a4050dd
jswini <jswinikoff@gmail.com>
2021-10-20 14:17:37 -0500

    added initializers and validators

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,74 +17,78 @@ class Rectangle(Base):
         self.x = x
         self.y = y
 
-        @property
-        def width(self):
-            """
-            returns the width property
-            """
-            return self.__width
+    @property
+    def width(self):
+        """
+        returns the width property
+        """
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """
-            sets the width property
-            """
+    @width.setter
+    def width(self, value):
+        """
+        sets the width property
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("width must be > 0")
         else:
             self.__width = value
 
 
-        @property
-        def height(self):
-            """
-            returns the height
-            """
-            return self.__width
+    @property
+    def height(self):
+        """
+        returns the height
+        """
+        return self.__width
 
-        @height.setter
-        def height(self, value):
-            """
-            sets the height
-            """
-            if type(value) is not int:
-                raise TypeError("height must be an integer")
-            elif value < 0:
-                raise ValueError("height must be > 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        """
+        sets the height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
-        @property
-        def x(self):
-            """
-            returns x
-            """
-            return self.__x
+    @property
+    def x(self):
+        """
+        returns x
+        """
+        return self.__x
 
-        @x.setter
-        def x(self, value):
-            """
-            sets x
-            """
-            if value < 0:
-                raise ValueError("x must be >= 0")
-            else:
-                self.__x = value
+    @x.setter
+    def x(self, value):
+        """
+        sets x
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
-        @property
-        def y(self):
-            """
-            returns y
-            """
-            return self.__y
+    @property
+    def y(self):
+        """
+        returns y
+        """
 
-        @y.setter
-        def y(self, value):
-            """
-            sets y
-            """
-            if value < 0:
-                raise ValueError("y must be >= 0")
-            else:
-                self.__y = value
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        sets y
+        """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
