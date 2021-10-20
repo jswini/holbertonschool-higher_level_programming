eccca1db215e92a9f43ed9b22e68c477bc6b604a
jswini <jswinikoff@gmail.com>
2021-10-20 11:45:39 -0700

    added remaining errors

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,27 +1,33 @@
 #!/usr/bin/python3
 """
+Defines a rectangle object that using superclass Base
 """
 from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Instantiate the rectangle
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super().__init__(id)
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.x = x
         self.y = y
 
         @property
         def width(self):
             """
+            returns the width property
             """
             return self.__width
 
         @width.setter
         def width(self, value):
             """
+            sets the width property
             """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -34,35 +40,51 @@ class Rectangle(Base):
         @property
         def height(self):
             """
+            returns the height
             """
             return self.__width
 
         @height.setter
         def height(self, value):
             """
+            sets the height
             """
+            if type(value) is not int:
+                raise TypeError("height must be an integer")
+            elif value < 0:
+                raise ValueError("height must be > 0")
             self.__height = value
 
         @property
         def x(self):
             """
+            returns x
             """
             return self.__x
 
         @x.setter
         def x(self, value):
             """
+            sets x
             """
-            self.__x = value
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            else:
+                self.__x = value
 
         @property
         def y(self):
             """
+            returns y
             """
             return self.__y
 
         @y.setter
         def y(self, value):
             """
+            sets y
             """
-            self.__y = value
+            if value < 0:
+                raise ValueError("y must be >= 0")
+            else:
+                self.__y = value
