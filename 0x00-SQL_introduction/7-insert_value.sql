f46e8e5bb356214846037b4d44241b029f1faa8f
jswini <jswinikoff@gmail.com>
2021-11-02 14:44:47 -0500

    corrected name

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This script will insert a row into the table
-INSERT INTO `first_table` (`id`,`name`) VALUES ("89","Best School")
+INSERT INTO `first_table` (`id`,`name`) VALUES ("89","Holberton School")
