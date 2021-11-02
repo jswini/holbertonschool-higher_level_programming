2e716ee4d78c8914acc35f539f43919141c160b9
jswini <jswinikoff@gmail.com>
2021-11-02 14:40:12 -0500

    changed text of name

--- 0x00-SQL_introduction/7-insert_value.sql
+++ 0x00-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- This script will insert a row into the table
-INSERT INTO `first_table` (`id`,`name`) VALUES ("89","Holberton")
+INSERT INTO `first_table` (`id`,`name`) VALUES ("89","Best School")
