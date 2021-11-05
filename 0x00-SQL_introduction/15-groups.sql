1b894d1569233c76028a9eee3dff7e9ac9a2ed47
jswini <jswinikoff@gmail.com>
2021-11-05 16:26:01 -0500

    added order by

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- this script displays score and number of times it appears in table
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
