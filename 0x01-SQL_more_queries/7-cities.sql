32bc55f96edc7af4d40ab144811b27f7a8cd058e
jswini <jswinikoff@gmail.com>
2021-11-15 12:58:30 -0600

    first commit cities

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- This script creates a database for the united states
+-- and a table for the cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT PRIMARY KEY AUTO_INCREMENT,
+state_id INT, FOREIGN KEY(state_id) REFERENCES states(id),
+name VARCHAR(256) NOT NULL
+)
