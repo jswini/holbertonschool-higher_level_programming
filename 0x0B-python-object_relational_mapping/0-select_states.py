881d039d74b716b205c8da73644bc989f0fc22d0
jswini <jswinikoff@gmail.com>
2021-11-16 15:46:31 -0600

    corrected permission of file

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -3,6 +3,6 @@
 This script prints the states listed in the table of select states sql
 """
 import MySQLdb
-db = MySQLdb.connect(host = local_host, user = root, passwd = root, db = hbtn_0e_0_usa)
+db = MySQLdb.connect(user = root, passwd = root, db = hbtn_0e_0_usa)
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
