239579047f1703df2825893de6ca098ef097c366
jswini <jswinikoff@gmail.com>
2021-11-16 15:18:48 -0600

    added order by

--- 0x01-SQL_more_queries/15-comedy_only.sql
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -5,4 +5,5 @@ INNER JOIN tv_show_genres
 on tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name = "Comedy";
+WHERE tv_genres.name = "Comedy"
+ORDER BY title ASC;
