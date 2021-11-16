d781031d07ff2538a50fe0e8b34afcf4f05dde98
jswini <jswinikoff@gmail.com>
2021-11-16 13:53:20 -0600

    fixed output by specifying condition

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,5 +2,5 @@
 SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
     FROM tv_shows
     RIGHT JOIN tv_show_genres
-    ON show_id
+    ON tv_shows.id = tv_show_genres.show_id
     ORDER BY tv_shows.title, tv_show_genres.genre_id;
