cffba0956fed0e868948e121a51989f45402f754
jswini <jswinikoff@gmail.com>
2021-11-16 15:02:10 -0600

    first commit of my genre

---
+++ 0x01-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- This script displays the genres of 1 show
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY name;
