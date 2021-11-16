28682f2687f10efbda0a4570f2941d23abdb29a8
jswini <jswinikoff@gmail.com>
2021-11-16 15:15:03 -0600

    first commit of comedy only

---
+++ 0x01-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- This script displays the comedies only
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+on tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy";
