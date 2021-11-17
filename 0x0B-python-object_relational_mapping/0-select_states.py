f8931a1baf5630eabd6fe31b7ccb2d375534b925
jswini <jswinikoff@gmail.com>
2021-11-17 13:30:12 -0600

    works now

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,11 @@
 """
 This script prints the states listed in the table of select states sql
 """
+from sys import argv
 import MySQLdb
-db = MySQLdb.connect(user = root, passwd = root, db = hbtn_0e_0_usa)
+db = MySQLdb.connect(user = argv[1], passwd = argv[2], db = argv[3])
 cur = db.cursor()
 cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+states = cur.fetchall()
+for i in states:
+    print(i)
