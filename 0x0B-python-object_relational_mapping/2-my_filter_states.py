5ae7e4c4296c28ec6c268c1cffcf6c1f2395c511
jswini <jswinikoff@gmail.com>
2021-11-17 14:45:58 -0600

    first commit filterstates by arg

---
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+This script filters by argument passed
+'''
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    item = argv[4]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name = '{}' \
+                ORDER BY states.id ASC;".format(item))
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    db.close()
