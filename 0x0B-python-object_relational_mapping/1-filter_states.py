9cf1d236f9e31cda4888b9387648530a21223b9b
jswini <jswinikoff@gmail.com>
2021-11-17 14:23:34 -0600

    first commit filter states

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+This script filters from the states database table
+'''
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY name;")
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    db.close()
