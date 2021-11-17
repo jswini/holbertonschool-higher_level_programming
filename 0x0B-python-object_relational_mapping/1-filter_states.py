721913a663c73f934a2bc78e7c7e143b291c6a9c
jswini <jswinikoff@gmail.com>
2021-11-17 14:35:51 -0600

    added ascending

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -7,8 +7,9 @@ import MySQLdb
 if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute(
-            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id;")
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE 'N%' \
+                ORDER BY states.id ASC;")
     states = cur.fetchall()
     for i in states:
         print(i)
