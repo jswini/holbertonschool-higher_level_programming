6564179d9d292df6d0d0c83aa0551b73c82088ec
jswini <jswinikoff@gmail.com>
2021-11-18 10:06:29 -0600

    added binary keyword

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states\
-                WHERE name LIKE 'N%' \
+                WHERE name LIKE BINARY 'N%' \
                 ORDER BY states.id ASC;")
     states = cur.fetchall()
     for i in states:
