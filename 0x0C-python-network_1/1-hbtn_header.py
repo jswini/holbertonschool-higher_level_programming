c5a6403024ed251b66e43febfdcf3faa50c8b57e
jswini <jswinikoff@gmail.com>
2021-12-04 14:19:16 -0600

    fixed to use args

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+import sys
 import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+with urllib.request.urlopen(sys.argv[1]) as response:
     html = response.read()
     urllib.response.addinfourl
 print(response.headers.get("X-Request-Id"))
