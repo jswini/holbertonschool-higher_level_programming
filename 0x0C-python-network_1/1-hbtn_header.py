f42043bc1700c4c91a2a2848250a64dea53f73fe
jswini <jswinikoff@gmail.com>
2021-12-04 14:27:12 -0600

    added include guard

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,7 +4,8 @@ This module gets the x request id from a website taken in as an arg
 '''
 import sys
 import urllib.request
-with urllib.request.urlopen(sys.argv[1]) as response:
-    html = response.read()
-    urllib.response.addinfourl
-print(response.headers.get("X-Request-Id"))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.read()
+        urllib.response.addinfourl
+    print(response.headers.get("X-Request-Id"))
