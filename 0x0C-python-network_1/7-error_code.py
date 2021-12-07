69b79bd560246a3a7180d86e66ec8815a157e8b0
jswini <jswinikoff@gmail.com>
2021-12-07 23:53:56 +0000

    changed output format

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     if req.status_code == 200:
         print("{}".format(req.text))
     else:
-        print("Error Code: {}".format(req.status_code))
+        print("Error code: {}".format(req.status_code))
