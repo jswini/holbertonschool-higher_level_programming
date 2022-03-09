b5ff1f716eadb0c1eb49a158732a75905dbe5fb4
jswini <jswinikoff@gmail.com>
2022-03-09 14:19:05 -0600

    adding test file

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+test file for Base
+"""
+import unittest
+from models.base import Base
+
+
+class testbase(unittest.TestCase):
+    """
+    tests for the base class
+    """
+    def test_init(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+        b2 = Base('a')
+        self.assertEqual(b2.id, 'a')
