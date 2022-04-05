82171634e61c5b6ff900c85c6b62dd026c8029ec
jswini <jswinikoff@gmail.com>
2022-04-05 12:54:51 -0500

    rebuilt 1 to not use array, changed original code for 1 to 6

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 /*
 This script is designed to output the contents of a var to the console
 */
-const langArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const lang of langArr) {
-  console.log(lang);
-}
+const lang = `C is fun
+Python is cool
+JavaScript is amazing`;
+console.log(lang);
---
+++ 0x00-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/*
+This script is designed to output the contents of a var to the console
+*/
+const langArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const lang of langArr) {
+  console.log(lang);
+}
