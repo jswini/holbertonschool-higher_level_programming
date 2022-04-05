bf59dcb77464912ecd7820764f715d398b1ba16f
jswini <jswinikoff@gmail.com>
2022-04-05 13:01:02 -0500

    changed comment

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-This script is designed to output the contents of a var to the console
+This script is designed to output the contents of a variable to the console
 */
 const lang = `C is fun
 Python is cool
