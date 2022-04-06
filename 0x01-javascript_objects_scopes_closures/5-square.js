9f0806523454112dd6dbdef9fe5b1d4cb82c44ce
jswini <jswinikoff@gmail.com>
2022-04-06 10:00:19 -0500

    first commit square

---
+++ 0x01-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    if ((size <= 0) || (isNaN(size))) {
+    // skipped
+    } else {
+      this.size = size;
+    }
+  }
+}
+
+module.exports = Square;
