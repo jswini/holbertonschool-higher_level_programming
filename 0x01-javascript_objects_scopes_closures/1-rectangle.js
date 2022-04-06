3d5594e4c3c2341cfbc4581ae9b97f026bbd44ce
jswini <jswinikoff@gmail.com>
2022-04-05 19:46:11 -0500

    first commit

---
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (h, w) {
+    this.height = h;
+    this.width = w;
+  }
+
+  displayRectangle () {
+    console.log('{}');
+  }
+}
+module.exports = Rectangle;
