265830f18a3931fb7f525ad4debb42d7bae68d35
jswini <jswinikoff@gmail.com>
2022-04-06 10:49:46 -0500

    changed import to different name

--- 0x01-javascript_objects_scopes_closures/6-square.js
+++ 0x01-javascript_objects_scopes_closures/6-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Square0 = require('./5-square');
 
-class Square extends Rectangle {
+class Square extends Square0 {
   constructor (size) {
     super(size, size);
   }
