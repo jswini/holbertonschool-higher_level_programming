30ff66761c1ad824f1391cf0069c44521dcbc0ad
jswini <jswinikoff@gmail.com>
2022-04-14 09:32:32 -0500

    changed to .include and just the number

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const path = process.argv[2];
-const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+const wedge = '/18/';
 request(path, (error, response, body) => {
   if (error) {
     console.error(error);
@@ -11,7 +11,7 @@ request(path, (error, response, body) => {
     for (const item in movies) {
       const characters = movies[item].characters;
       for (const charURL in characters) {
-        if (characters[charURL] === wedge) {
+        if (characters[charURL].includes(wedge)) {
           count += 1;
         }
       }
